1e5fdcda766b819b5affa3a7cf0e566d48c99024
arthur.valente.reis <arthur.valente.reis@gmail.com>
2020-06-04 01:58:51 -0300

    create lading page and register new point

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,26 +1,40 @@
-import React from 'react';
-import logo from './logo.svg';
+// import React, { useState } from 'react';
+// import './App.css';
+
+// //JSX - Síntaxe de XML dentro do javascript
+// import Header from './Header';
+
+// function App() {
+//   const [counter, setCounter] = useState(0);
+
+//   function handleBottomClick(){
+//     setCounter(counter + 1 );
+//   }
+//   return (
+//       <div>
+//         <Header title="Ecoleta" />     
+
+//         <h4>{counter}</h4>
+//         <button type="button" onClick={handleBottomClick}>Aumentar</button>   
+//       </div>
+//   );
+// }
+
+// export default App;
+
+
+
+import React, { useState } from 'react';
 import './App.css';
+
+import Routes from './routes';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div>
+          <Routes />
+      </div>
   );
 }
-
 export default App;
+
