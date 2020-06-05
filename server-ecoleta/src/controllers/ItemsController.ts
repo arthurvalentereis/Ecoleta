6e23daafcc4e0fa74cc953c54d2ed28a81589b39
arthur.valente.reis <arthur.valente.reis@gmail.com>
2020-06-05 02:56:15 -0300

    mobile version

--- server-ecoleta/src/controllers/ItemsController.ts
+++ server-ecoleta/src/controllers/ItemsController.ts
@@ -11,7 +11,7 @@ class ItemsController {
             return {
                 id: items.id,
                 title: items.title,
-                image_url: `http://localhost:3333/uploads/${items.image}`,
+                image_url: `http://192.168.1.6:3333/uploads/${items.image}`,
             }
         });
         return response.json(serializedItems);
